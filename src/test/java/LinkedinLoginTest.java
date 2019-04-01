a616021f2946a39e0e3c630649b8beb29799e59f
KATERYNA BORSHE <katrella@meta.ua>
2019-04-01 19:02:14 +0300

    Negative case for linkedin.com

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -31,7 +31,101 @@ public class LinkedinLoginTest {
     }
 
     @Test
-    public void negativeWithEmptyValuesTest(){
+    public void negativeWithEmptyValuesTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+
+        WebElement emailField =
+                driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement passwordField =
+                driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton =
+                driver.findElement(By.xpath("//input[@id='login-submit']"));
+
+        emailField.sendKeys("");
+        passwordField.sendKeys("");
+        signInButton.click();
+        Assert.assertEquals(driver.getTitle(),
+                "LinkedIn: Войти или зарегистрироваться",
+                "Start page title is wrong.");
+
+        driver.quit();
 
     }
+
+    @Test
+    public void negativeWithInvalidEmailValuesTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+
+        WebElement emailField =
+                driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement passwordField =
+                driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton =
+                driver.findElement(By.xpath("//input[@id='login-submit']"));
+
+        emailField.sendKeys("qwerty");
+        passwordField.sendKeys("qwerty");
+        signInButton.click();
+        WebElement errorMessage =
+                driver.findElement(By.xpath("//div[@id='error-for-username']"));
+        Assert.assertEquals(errorMessage.getText(), "Укажите действительный адрес эл. почты.",
+                "Incorrect error message with invalid Email");
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void negativeWithValidUnregictredEmailValuesTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+
+        WebElement emailField =
+                driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement passwordField =
+                driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton =
+                driver.findElement(By.xpath("//input[@id='login-submit']"));
+
+        emailField.sendKeys("qwerty@gmail.com");
+        passwordField.sendKeys("qwerty");
+        signInButton.click();
+        WebElement errorMessage =
+                driver.findElement(By.xpath("//div[@id='error-for-username']"));
+        Assert.assertEquals(errorMessage.getText(), "Этот адрес эл. почты не зарегистрирован в LinkedIn. Повторите попытку.",
+                "Incorrect error message with valid unregictred Email");
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void negativeWithValidInvalidPaswordValuesTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+
+        WebElement emailField =
+                driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement passwordField =
+                driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton =
+                driver.findElement(By.xpath("//input[@id='login-submit']"));
+
+        emailField.sendKeys("linkedin.tst.yanina@gmail.com");
+        passwordField.sendKeys("qwerty");
+        signInButton.click();
+        WebElement errorMessagePassword =
+                driver.findElement(By.xpath("//div[@id='error-for-password']"));
+        Assert.assertEquals(errorMessagePassword.getText(), "Это неверный пароль. Повторите попытку или измените пароль.",
+                "Incorrect error message with invalid Password");
+
+        driver.quit();
+
+    }
+
+
 }
+
+
